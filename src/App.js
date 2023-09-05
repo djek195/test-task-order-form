e7f8fca18309022ddd01978beaa4fab24ddb2308
Yevhenii <djek195@gmail.com>
2023-09-05 19:12:39 +0300

    added empty future components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { useRef, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Header from './components/Header/Header';
 import Grid from '@mui/material/Unstable_Grid2';
 import Paper from '@mui/material/Paper';
+import Header from './components/Header/Header';
+import OrderSummary from './components/OrderSummary/OrderSummary';
+import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
+import Shipping from './components/Shipping/Shipping';
+import Billing from './components/Billing/Billing';
+import Payment from './components/Payment/Payment';
 import './App.css';
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -14,7 +20,7 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
-  height: '100%',
+  height: '600px',
 }));
 
 function App() {
@@ -28,43 +34,70 @@ function App() {
 
   return (
     <div className="App">
-      <Header toggleModel={toggleModalWindow}></Header>
-      <Box ref={rootRef}>
-        <Modal
-          disablePortal
-          disableEnforceFocus
-          disableAutoFocus
-          hideBackdrop
-          open={modalView}
-          aria-labelledby="server-modal-title"
-          aria-describedby="server-modal-description"
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          container={() => rootRef.current}
-        >
-          <Grid
-            container
+      <BrowserRouter>
+        <Header toggleModel={toggleModalWindow}></Header>
+        <Box ref={rootRef}>
+          <Modal
+            disablePortal
+            disableEnforceFocus
+            disableAutoFocus
+            hideBackdrop
+            open={modalView}
+            aria-labelledby="server-modal-title"
+            aria-describedby="server-modal-description"
             sx={{
-              position: 'relative',
-              width: '700px',
-              height: '600px',
-              bgcolor: 'background.paper',
-              borderRadius: '10px',
-              boxShadow: (theme) => theme.shadows[5],
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
             }}
+            container={() => rootRef.current}
           >
-            <Grid xs={8}>
-              <Item>Order form</Item>
+            <Grid
+              container
+              sx={{
+                position: 'relative',
+                width: '700px',
+                height: 'auto',
+                bgcolor: 'background.paper',
+                borderRadius: '5px',
+                paddingY: '25px',
+                boxShadow: (theme) => theme.shadows[5],
+              }}
+            >
+              <Grid
+                xs={7}
+                sx={{
+                  paddingX: '50px',
+                }}
+              >
+                <Breadcrumbs />
+                <Routes>
+                  <Route
+                    path={'/'}
+                    element={<Shipping closeModal={toggleModalWindow} />}
+                  />
+                  <Route
+                    path={'/billing'}
+                    element={<Billing closeModal={toggleModalWindow} />}
+                  />
+                  <Route
+                    path={'/payment'}
+                    element={<Payment closeModal={toggleModalWindow} />}
+                  />
+                </Routes>
+              </Grid>
+              <Grid
+                xs={5}
+                sx={{
+                  paddingX: '25px',
+                }}
+              >
+                <OrderSummary />
+              </Grid>
             </Grid>
-            <Grid xs={4}>
-              <Item>Products</Item>
-            </Grid>
-          </Grid>
-        </Modal>
-      </Box>
+          </Modal>
+        </Box>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/OrderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Stack, Typography, Link } from '@mui/material';
+
+const OrderSummary = () => {
+  return (
+    <Stack >
+      <Stack
+        sx={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          
+          
+        }}
+      >
+        <Typography>Order Summary</Typography>
+        <Link>edit order</Link>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default OrderSummary;
