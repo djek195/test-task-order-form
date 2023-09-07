0ee95ef9b617a323b888cf26d297f592c4bceff7
Yevhenii <djek195@gmail.com>
2023-09-07 12:26:18 +0300

    updated breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -2,47 +2,44 @@ import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { useLocation } from 'react-router-dom';
-
-function handleClick(event) {
-  event.preventDefault();
-  console.info('You clicked a breadcrumb.');
-}
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function CustomSeparator() {
   const location = useLocation();
+  const navigate = useNavigate();
   const currentPath = location.pathname; //
-
 
   const isShippingActive = currentPath === '/';
   const isBillingActive = currentPath === '/billing';
   const isPaymentActive = currentPath === '/payment';
 
+  const handleClick = (path) => (event) => {
+    event.preventDefault();
+    navigate(path);
+  };
+
   const breadcrumbs = [
     <Link
-      underline={isShippingActive ? "none" : "hover"}
+      underline={isShippingActive ? 'none' : 'hover'}
       key="1"
-      color={isShippingActive ? "text.primary" : "inherit"} 
-      href="/"
-      onClick={handleClick}
+      color={isShippingActive ? 'text.primary' : 'inherit'}
+      onClick={handleClick('/')}
     >
       Shipping
     </Link>,
     <Link
-      underline={isBillingActive ? "none" : "hover"}
+      underline={isBillingActive ? 'none' : 'hover'}
       key="2"
-      color={isBillingActive ? "text.primary" : "inherit"}
-      href="/billing"
-      onClick={handleClick}
+      color={isBillingActive ? 'text.primary' : 'inherit'}
+      onClick={handleClick('/billing')}
     >
       Billing
     </Link>,
     <Link
-      underline={isPaymentActive ? "none" : "hover"}
+      underline={isPaymentActive ? 'none' : 'hover'}
       key="3"
-      color={isPaymentActive ? "text.primary" : "inherit"}
-      href="/payment"
-      onClick={handleClick}
+      color={isPaymentActive ? 'text.primary' : 'inherit'}
+      onClick={handleClick('/payment')}
     >
       Payment
     </Link>,
